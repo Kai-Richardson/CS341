71cff7c870d2a082faf061fe765496a2a7f7cb07
Kai Richardson <richardk22@up.edu>
2021-09-13 18:00:50 -0700

    External file

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+jQuery('#order').click(function () {
+    const notes = $("#notes");
+    if (notes.val().toLowerCase().includes("vegan")) {
+        alert("Warning: Our Cheesecakes contain dairy!")
+    } else {
+        document.getElementById("order-zone").style.visibility = "hidden"
+        document.getElementById("thanks").style.visibility = "visible"
+
+        const cake_num = $("#quantity-cakes option:selected").text();
+        const cake_type = $('input[name="topping"]:checked').val() ?? "None";
+        const cake_notes = notes.val();
+
+        $("#thanks-details").text(`Quantity: ${cake_num} | Topping: ${cake_type} | Notes: ${cake_notes}`)
+    }
+});
+
+jQuery("#select-menu ul li").click(function(){
+    $("#select-menu ul li").css("color","inherit");
+    $(this).css("color","rebeccapurple");
+    const selected = $(this).text()
+    $('#selected-month').text(`${selected}`)
+});
